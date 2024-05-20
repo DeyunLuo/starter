ba72c4bcd95be09be9231a3c23fe4992a5d72c2f
deyun.luo <deyun.luo@baishan.com>
2024-05-20 21:37:39 +0800

    feat: add python lazyextra

--- lua/plugins/golang.lua
+++ lua/plugins/golang.lua
@@ -4,7 +4,6 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        -- pyright will be automatically installed with mason and loaded with lspconfig
         pyright = {},
         gopls = {
           cmd = { "gopls", "serve" },
@@ -24,25 +23,3 @@ return {
     },
   },
 }
--- local util = require("lspconfig.util")
--- return {
---   -- file explorer
---   {
---     "neovim/nvim-lspconfig",
---     setup = {
---       require("lspconfig").gopls.setup({
---         cmd = { "gopls", "serve" },
---         filetypes = { "go", "gomod" },
---         root_dir = util.root_pattern("go.work", "go.mod", ".git"),
---         settings = {
---           gopls = {
---             analyses = {
---               unusedparams = true,
---             },
---             staticcheck = true,
---           },
---         },
---       }),
---     },
---   },
--- }
